ee51340dc2c6b34c3910a381ec2865ffac6a0c37
Pluto <sand.warrior@hotmail.com>
2020-05-28 01:36:57 -0400

    Updates

---
+++ precache-manifest.441232b81a8286ddf4d5998759ca6aad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a574e530b1b2c349b5bb2c2df9604e9b",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "44fafee20c58ab906298",
+    "url": "/home/static/css/2.aec26d01.chunk.css"
+  },
+  {
+    "revision": "3c2582fae9b603295720",
+    "url": "/home/static/css/main.3df0f7f8.chunk.css"
+  },
+  {
+    "revision": "44fafee20c58ab906298",
+    "url": "/home/static/js/2.25c09916.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/home/static/js/2.25c09916.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c2582fae9b603295720",
+    "url": "/home/static/js/main.c1562de8.chunk.js"
+  },
+  {
+    "revision": "7a08832364173717111e",
+    "url": "/home/static/js/runtime-main.78d57765.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.23ab765775ef499e5ddbef6a28375542.js"
+  "/home/precache-manifest.441232b81a8286ddf4d5998759ca6aad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
