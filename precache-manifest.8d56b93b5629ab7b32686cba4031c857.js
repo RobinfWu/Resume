24c7cde9f6180b21423e332398cbcc4e93a3292b
Pluto <sand.warrior@hotmail.com>
2020-05-27 22:21:34 -0400

    Updates

---
+++ precache-manifest.8d56b93b5629ab7b32686cba4031c857.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a59b4fd44946419a81ed42ab69f04cf",
+    "url": "/Resume/index.html"
+  },
+  {
+    "revision": "d1ffab7e537059fc522d",
+    "url": "/Resume/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d3b3b7e629bf9bd4d3c8",
+    "url": "/Resume/static/js/2.df5d811d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Resume/static/js/2.df5d811d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1ffab7e537059fc522d",
+    "url": "/Resume/static/js/main.0a03649e.chunk.js"
+  },
+  {
+    "revision": "f3187c13572c15a6eba5",
+    "url": "/Resume/static/js/runtime-main.2839ef2e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
